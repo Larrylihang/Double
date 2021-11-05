21b7e25e32b4dd91d6b2282939e27284fc948455
larry <l030411h@student.staffs.ac.uk>
2021-11-05 14:59:22 +0000

    reverse

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 /*
  * @Author: your name
  * @Date: 2021-10-27 01:41:42
- * @LastEditTime: 2021-11-05 14:53:58
- * @LastEditors: your name
+ * @LastEditTime: 2021-11-05 14:58:30
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /practiceProject/routes/web.php
  */
@@ -68,5 +68,3 @@ Route::group(['middleware' => ['web', 'login.check'],'prefix'=>'admin', 'namespa
 Route::fallback(function () {
     return redirect('/');
 });
-
-//thisaosfoiad
